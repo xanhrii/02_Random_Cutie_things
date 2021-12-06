8c84d13f9657c86c5cc3301ea3c480af06789a77
xanhrii <17521246@gm.uit.edu.vn>
2021-12-06 15:06:45 +0700

    #02_random cutie things hihi

---
+++ index.js
@@ -0,0 +1,207 @@
+const quotes = [
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #01',
+            img: 'https://i.pinimg.com/564x/7f/05/70/7f0570aa4fb25b2fcb3a834a76f5cb59.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #02',
+            img: 'https://i.pinimg.com/564x/33/8a/dc/338adc308dcadcb02e2e279d5fd88d4b.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #03',
+            img: 'https://i.pinimg.com/564x/54/9c/d4/549cd4d50ea9ee3c7c720591adaaea75.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #04',
+            img: 'https://i.pinimg.com/564x/af/4b/c6/af4bc6d1bfbd84aa18677a4c539683ff.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #05',
+            img: 'https://i.pinimg.com/564x/fd/65/2f/fd652fe42be1e634230570d1c6b1e560.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #06',
+            img: 'https://i.pinimg.com/564x/19/db/1d/19db1ddfceaf3b436405e9209f7573ec.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #07',
+            img: 'https://i.pinimg.com/564x/83/e5/f9/83e5f9f7790c1cb20bca026f691e91fb.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #08',
+            img: 'https://i.pinimg.com/564x/63/12/b3/6312b39550ae8e31a7ba9b7f988b14a4.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #09',
+            img: 'https://i.pinimg.com/564x/82/0c/fb/820cfbbb1fd63aab1a3e6ddb67748580.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #10',
+            img: 'https://i.pinimg.com/564x/09/b5/38/09b53851bce85f3d242935b7922352da.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #011',
+            img: 'https://i.pinimg.com/564x/52/dd/08/52dd08ff89635e3c11249c03d47b556f.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #012',
+            img: 'https://i.pinimg.com/564x/db/07/9b/db079bae77db113da35a186561375574.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #013',
+            img: 'https://i.pinimg.com/564x/ce/80/3f/ce803fac6770ebfb4badc6341e028f09.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #014',
+            img: 'https://i.pinimg.com/564x/70/8f/4d/708f4dd339d6ec94c7f988de18f9ac7a.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #015',
+            img: 'https://i.pinimg.com/564x/68/2b/d0/682bd0e79d381b2e7d98e9a44b521137.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #016',
+            img: 'https://i.pinimg.com/564x/05/91/18/0591180bcbe54d3dcf38b3ac042a01d7.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #017',
+            img: 'https://i.pinimg.com/564x/e1/dd/52/e1dd52b9a482b2e288b480c6414214c7.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #018',
+            img: 'https://i.pinimg.com/564x/34/30/51/3430519038e1e63b9e5da2c0a7ef6c57.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #019',
+            img: 'https://i.pinimg.com/564x/0f/42/91/0f429138875d91d027937327b5e226ec.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #020',
+            img: 'https://i.pinimg.com/564x/f4/44/ef/f444ef6f06ac6c55bfa5e228145c9c74.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #021',
+            img: 'https://i.pinimg.com/564x/0f/98/dc/0f98dcd2053c056341918c0ad553919e.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #022',
+            img: 'https://i.pinimg.com/564x/d4/f4/ab/d4f4ab67a92b4541c555508538667249.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #023',
+            img: 'https://i.pinimg.com/564x/e1/cb/80/e1cb80c4ec813077c1a4b4f5bd2f2f27.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #024',
+            img: 'https://i.pinimg.com/564x/d6/5b/c1/d65bc1fdba8bcf9c971d6ca3c0af0ad6.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #025',
+            img: 'https://i.pinimg.com/564x/4f/63/ce/4f63ce7aa758689d58b97bbadd07eb2c.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #026',
+            img: 'https://i.pinimg.com/564x/74/14/e4/7414e49d919477723e5fa3b31f233768.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #027',
+            img: 'https://i.pinimg.com/564x/ed/9a/3f/ed9a3fd14abcdc0c7e1aec331097c30d.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #028',
+            img: 'https://i.pinimg.com/564x/85/eb/af/85ebaf82ef9e6f264dd7e3892b34afd6.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #029',
+            img: 'https://i.pinimg.com/564x/0c/fe/96/0cfe96a36e6eebd790c2cd9964dff5a2.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #030',
+            img: 'https://i.pinimg.com/564x/f4/44/ef/f444ef6f06ac6c55bfa5e228145c9c74.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #031',
+            img: 'https://i.pinimg.com/564x/52/23/b3/5223b3e61b8812e7870348260131d286.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #032',
+            img: 'https://i.pinimg.com/564x/19/f6/3f/19f63f90d07b85d305e3394bdd854bd4.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #033',
+            img: 'https://i.pinimg.com/564x/f4/fb/d2/f4fbd2287b33886ee2c0827cb3068a1f.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #034',
+            img: 'https://i.pinimg.com/564x/2a/c7/40/2ac740d1a9fc702c6ef15a9e6e8e3d3e.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #035',
+            img: 'https://i.pinimg.com/564x/b1/ed/3d/b1ed3de357226c19613125f26e94ac0f.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #036',
+            img: 'https://i.pinimg.com/564x/f4/b7/48/f4b748aa299f73176266780b99d75229.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #037',
+            img: 'https://i.pinimg.com/564x/f1/38/f3/f138f3010fea30446ac994da740c3062.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #038',
+            img: 'https://i.pinimg.com/564x/63/dd/1a/63dd1ad36b30fc39f24a88f326d89944.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #039',
+            img: 'https://i.pinimg.com/564x/99/c5/01/99c501a02cea4235e7496ee2fdac709e.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #040',
+            img: 'https://i.pinimg.com/564x/ba/c9/1b/bac91b058a184f008277c4afe4fe46e4.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #041',
+            img: 'https://i.pinimg.com/564x/ce/18/63/ce1863caefa7e8e515c830b61d5c9c25.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #042',
+            img: 'https://i.pinimg.com/564x/ce/55/8e/ce558ef44c213408cfcaca4d577ea75e.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #043',
+            img: 'https://i.pinimg.com/564x/a3/81/a0/a381a01b716f03397d7f043e42aee31a.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #044',
+            img: 'https://i.pinimg.com/564x/dd/04/bf/dd04bfaef08c40e259017aca04685630.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #045',
+            img: 'https://i.pinimg.com/564x/60/5b/b4/605bb436c4bb052dd742630d9be1c1a6.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #046',
+            img: 'https://i.pinimg.com/564x/d1/99/b3/d199b3f90d73117bd419a352155e6019.jpg'
+      },
+      {
+            name: 'Vì bạn cute giống tui, nên tui mí share cho bạn chiếc cute này ó nho, nè he #047',
+            img: 'https://i.pinimg.com/564x/f5/9f/f3/f59ff35780cdceafd3c6e3e6ea625394.jpg'
+      },
+]
+
+const change = document.getElementById('change')
+const quote = document.getElementById('content-quote')
+const image = document.getElementById('img')
+const helo= document.getElementById('helo')
+
+change.addEventListener('click', HandleChange)
+
+function HandleChange(){
+      const randomquote = quotes[Math.floor(Math.random()*quotes.length)]
+      const curQuote = quote.innerText;
+      if(curQuote != randomquote.name){
+            quote.innerHTML = randomquote.name;
+            image.src = randomquote.img;
+            image.style.display='block';
+      }
+}
